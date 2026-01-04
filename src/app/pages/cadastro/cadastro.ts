a3faca76ab206f55f919fad8872e1974de6aadbc
Caio <caiovictorarantes@gmai.com>
2026-01-04 16:00:31 -0300

    Add Cadastro page with form validation and routing

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from "@angular/forms";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {
+export class Cadastro implements OnInit {
+
+  cadastroForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.cadastroForm = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      cpf: ['', [Validators.required, Validators.minLength(11)]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarSenha: ['', Validators.required]
+    }, { validators: this.senhasIguais });
+  }
+
+  senhasIguais(control: AbstractControl): ValidationErrors | null {
+    const senha = control.get('senha');
+    const confirmarSenha = control.get('confirmarSenha');
+    
+    if (senha && confirmarSenha && senha.value !== confirmarSenha.value) {
+      return { senhasDiferentes: true };
+    }
+    
+    return null;
+  }
+
+  cadastrar() {
+    if(this.cadastroForm.valid) {
+      console.log("Formulário válido enviado:", this.cadastroForm.value);
+    } else {
+      this.cadastroForm.markAllAsTouched();
+      console.log("Formulário inválido.");
+    }
+
+    console.log(this.cadastroForm.value);
+  }
 
 }
